d3645112caee300dacbf6a01eac74fe1c3df7a4f
Nick Cardoza <48460641+nacardo@users.noreply.github.com>
2021-06-23 19:17:05 -0500

    Update vector.js

--- vector.js
+++ vector.js
@@ -1,32 +1,3 @@
-//
-//
-// function setup() {
-//   createCanvas(windowWidth, windowHeight);
-//
-// }
-//
-// function draw() {
-//   background(0);
-//
-//   let pos = createVector(windowWidth / 2, windowHeight /2);
-//   let mouse = createVector(mouseX, mouseY);
-//
-//   let v = p5.Vector.sub(mouse, pos);
-//
-//   // let m = v.mag();
-//   // v.div(m);
-//   // v.normalize();
-//   // v.mult(50);
-//   v.setMag(100);
-//
-//   translate(width/2, height/2);
-//   strokeWeight(4);
-//   stroke(255);
-//   line(0, 0, v.x, v.y);
-//
-// }
-
-
 var slider;
 
 function setup() {
